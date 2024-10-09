5959bb2cb534d80ee1905ed15c55fd06d71ed807
Lsy <379338943@qq.com>
2024-10-09 17:06:50 +0800

    修改命名

--- Runtime/YIUIConstAsset_3DDisplay.cs
+++ Runtime/YIUIConstAsset_3DDisplay.cs
@@ -6,12 +6,8 @@ namespace YIUIFramework
 {
     public partial class YIUIConstAsset
     {
-        #region 3DDisplay
-
         [BoxGroup("3DDisplay", CenterLabel = true)]
         [LabelText("3D显示层名称")]
         public string YIUI3DLayer = "YIUI3DLayer";
-
-        #endregion
     }
 }
